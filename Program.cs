e40b0fc48044ab93d4e0b054d4ad0c0d111d9806
SensibleHoneybee <sensible.honeybee@yahoo.com>
2022-08-20 21:00:58 +0100

    Initial commit of .NET worker app code

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using DigitalMakerWorkerApp;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
